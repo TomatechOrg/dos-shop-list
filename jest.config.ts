55bfbbec53f7c61c8462b615c71bd4642b1b7224
TomatechGames <tomatechgames@gmail.com>
2025-07-25 22:09:44 +0100

    convert test from ts to js

--- jest.config.ts
+++ jest.config.ts
@@ -23,6 +23,7 @@ const config: Config = {
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
+  
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
--- src/app/__test__/api.test.jsx
+++ src/app/__test__/api.test.jsx
@@ -6,6 +6,13 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ user: { id: 1, name: 'John Fortnite' } }),
+  })
+);
+
 describe('Home', () => {
   it('has edit button', () => {
     render(<Home />)
